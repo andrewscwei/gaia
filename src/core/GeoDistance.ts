541c07f5a46b4d5df2729993f2f5f23a54457fa4
Andrew Wei <andrewscwei@gmail.com>
2019-06-07 15:13:30 -0700

    Fix unit conversions

--- src/core/GeoDistance.ts
+++ src/core/GeoDistance.ts
@@ -49,8 +49,8 @@ export default class GeoDistance {
       this.nauticalMiles = value * 0.53995680;
       this.yards = value * 1093.6133;
       this.inches = value * 39370.079;
-      this.degrees = value * EARTH_RADIUS_IN_METERS * 180 / Math.PI;
-      this.radians = value * EARTH_RADIUS_IN_METERS;
+      this.degrees = ((value * 1000) / EARTH_RADIUS_IN_METERS) * (180 / Math.PI);
+      this.radians = (value * 1000) / EARTH_RADIUS_IN_METERS;
     case 'miles':
       this.meters = value * 1609.344;
       this.kilometers = value * 1.609344;
@@ -58,8 +58,8 @@ export default class GeoDistance {
       this.nauticalMiles = value * 0.86897624;
       this.yards = value * 1760;
       this.inches = value * 63360;
-      this.degrees = value * 1.609344 * EARTH_RADIUS_IN_METERS * 180 / Math.PI;
-      this.radians = value * 1.609344 * EARTH_RADIUS_IN_METERS;
+      this.degrees = ((value * 1609.344) / EARTH_RADIUS_IN_METERS) * (180 / Math.PI);
+      this.radians = (value * 1609.344) / EARTH_RADIUS_IN_METERS;
     case 'nauticalmiles':
       this.meters = value * 1852;
       this.kilometers = value * 1.852;
@@ -67,8 +67,8 @@ export default class GeoDistance {
       this.nauticalMiles = value;
       this.yards = value * 2025.3718;
       this.inches = value * 72913.386;
-      this.degrees = value * 1.852 * EARTH_RADIUS_IN_METERS * 180 / Math.PI;
-      this.radians = value * 1.852 * EARTH_RADIUS_IN_METERS;
+      this.degrees = ((value * 1852) / EARTH_RADIUS_IN_METERS) * (180 / Math.PI);
+      this.radians = (value * 1852) / EARTH_RADIUS_IN_METERS;
     case 'inches':
       this.meters = value * 0.0254;
       this.kilometers = value * 2.54 * 1e-5;
@@ -76,8 +76,8 @@ export default class GeoDistance {
       this.nauticalMiles = value * 1.3714903 * 1e-5;
       this.yards = value * 0.027777778;
       this.inches = value;
-      this.degrees = value * 2.54 * 1e-5 * EARTH_RADIUS_IN_METERS * 180 / Math.PI;
-      this.radians = value * 2.54 * 1e-5 * EARTH_RADIUS_IN_METERS;
+      this.degrees = ((value * 0.0254) / EARTH_RADIUS_IN_METERS) * (180 / Math.PI);
+      this.radians = (value * 0.0254) / EARTH_RADIUS_IN_METERS;
     case 'yards':
       this.meters = value * 0.9144;
       this.kilometers = value * 0.0009144;
@@ -85,26 +85,26 @@ export default class GeoDistance {
       this.nauticalMiles = value * 0.00049373650;
       this.yards = value;
       this.inches = value * 36;
-      this.degrees = value * 0.0009144 * EARTH_RADIUS_IN_METERS * 180 / Math.PI;
-      this.radians = value * 0.0009144 * EARTH_RADIUS_IN_METERS;
+      this.degrees = ((value * 0.9144) / EARTH_RADIUS_IN_METERS) * (180 / Math.PI);
+      this.radians = (value * 0.9144) / EARTH_RADIUS_IN_METERS;
     case 'radians':
-      this.meters = value / EARTH_RADIUS_IN_METERS * 1000;
-      this.kilometers = value / EARTH_RADIUS_IN_METERS;
-      this.miles = value / EARTH_RADIUS_IN_METERS * 0.62137119;
-      this.nauticalMiles = value / EARTH_RADIUS_IN_METERS * 0.53995680;
-      this.yards = value / EARTH_RADIUS_IN_METERS * 1093.6133;
-      this.inches = value / EARTH_RADIUS_IN_METERS * 39370.079;
-      this.degrees = value * 180 / Math.PI;
+      this.meters = value * EARTH_RADIUS_IN_METERS;
+      this.kilometers = (value * EARTH_RADIUS_IN_METERS) / 1000;
+      this.miles = (value * EARTH_RADIUS_IN_METERS) * 0.00062137;
+      this.nauticalMiles = (value * EARTH_RADIUS_IN_METERS) * 0.00053995680;
+      this.yards = (value * EARTH_RADIUS_IN_METERS) * 1.0936;
+      this.inches = (value * EARTH_RADIUS_IN_METERS) * 39.370;
+      this.degrees = value * (180 / Math.PI);
       this.radians = value;
     case 'degrees':
-      this.meters = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS * 1000;
-      this.kilometers = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS;
-      this.miles = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS * 0.62137119;
-      this.nauticalMiles = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS * 0.53995680;
-      this.yards = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS * 1093.6133;
-      this.inches = value * Math.PI / 180 / EARTH_RADIUS_IN_METERS * 39370.079;
+      this.meters = (value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS;
+      this.kilometers = ((value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS) * 0.001;
+      this.miles = ((value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS) * 0.00062137;
+      this.nauticalMiles = ((value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS) * 0.00053995680;
+      this.yards = ((value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS) * 1.0936;
+      this.inches = ((value * (Math.PI / 180)) * EARTH_RADIUS_IN_METERS) * 39.370;
       this.degrees = value;
-      this.radians = value * Math.PI / 180;
+      this.radians = value * (Math.PI / 180);
     default:
       this.meters = value;
       this.kilometers = value * 0.001;
