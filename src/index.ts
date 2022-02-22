67755c01990214ad5e2f8ac16bb948e7cbd56398
Andrew Wei <andrewscwei@gmail.com>
2022-02-21 17:21:06 -0800

    Upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/** @license gaia
+ * © Andrew Wei
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
 export * from './core'
 export * from './types'
 export * from './utils/debug'
